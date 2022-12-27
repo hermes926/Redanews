40c9067483d07e1e0569243824f94ae47eaf19ad
ChienYu-1215 <b08705034@ntu.edu.tw>
2022-12-27 23:27:16 +0800

    [FE] Update themes

--- frontend/src/Theme/index.js
+++ frontend/src/Theme/index.js
@@ -1,10 +1,22 @@
 import { extendTheme } from "@chakra-ui/react";
+import Components from "./Components";
 
 const theme = extendTheme({
   fonts: {
     heading: `'Montserrat', sans-serif`,
     body: `'Montserrat', sans-serif`,
   },
+  colors: {
+    transparent: "transparent",
+    black: "#000",
+    white: "#fff",
+    blue: {
+      700: "#2C5282",
+      800: "#2A4365",
+      900: "#1A365D",
+    },
+  },
+  Components,
 });
 
 export default theme;
