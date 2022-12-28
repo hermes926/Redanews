8143f63103926e0c79b4f7d6ca29540384657f56
zombie5454 <b08902129@ntu.edu.tw>
2022-12-28 18:02:33 +0800

    comment out Body

--- frontend/src/Containers/App.js
+++ frontend/src/Containers/App.js
@@ -4,7 +4,7 @@ import Paper from "@material-ui/core/Paper";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Header from "../Components/Header";
-import Body from "./Body";
+//import Body from "./Body";
 import Home from "./Home";
 import Login from "./Accounts/Login";
 import SignUp from "./Accounts/SignUp";
@@ -18,7 +18,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/game" element={<Body />} />
+          {/* waiting for Body complete
+          <Route path="/game" element={<Body />} />*/}
         </Routes>
       </Router>
     </>
