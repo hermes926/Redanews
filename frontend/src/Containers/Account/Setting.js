3748e5262db78b08b65f3bff34bee5e6c75bab77
ChienYu-1215 <b08705034@ntu.edu.tw>
2022-12-29 17:43:31 +0800

    [FE] Open up Setting page (blank)

---
+++ frontend/src/Containers/Account/Setting.js
@@ -0,0 +1,14 @@
+// React Utils, UI Components
+import { Flex, Box } from "@chakra-ui/react";
+
+// User-defined Components, Container
+
+const Setting = () => {
+  return (
+    <Flex height="90vh" align="center" justify="center">
+      <Box bgColor="pink" width="100%" height="90vh" position="absolute" />
+    </Flex>
+  );
+};
+
+export default Setting;
