a4f64b0ae2377f22dc9c8f8a6b2ee38a5984dcfb
ChienYu-1215 <b08705034@ntu.edu.tw>
2022-12-29 21:22:33 +0800

    [FE] Password Setting Page

---
+++ frontend/src/Components/account/PasswordSetting.js
@@ -0,0 +1,311 @@
+// React Utils, UI Components
+// React Utils, UI Components
+import { useState } from "react";
+import {
+  Flex,
+  Box,
+  Stack,
+  Center,
+  Text,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Icon,
+  IconButton,
+  useToast,
+} from "@chakra-ui/react";
+import { FiLock, FiCheck } from "react-icons/fi";
+import { HiEye, HiEyeOff } from "react-icons/hi";
+
+const PasswordSetting = ({ user }) => {
+  const [orgPassword, setOrgPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [isOpen, setIsOpen] = useState({
+    orgPassword: false,
+    newPassword: false,
+    confirmPassword: false,
+  });
+
+  const displayToast = useToast();
+
+  const onSubmit = () => {};
+
+  const onCancel = () => {
+    displayToast({
+      title: "Cancel Passwords Changing",
+      status: "success",
+      duration: 2000,
+      isClosable: true,
+    });
+    setOrgPassword("");
+    setNewPassword("");
+    setConfirmPassword("");
+  };
+
+  const onClickReveal = (type) => {
+    let newState = isOpen;
+    if (type === "orgPassword") {
+      newState.orgPassword = !isOpen.orgPassword;
+      setIsOpen(newState);
+    }
+    if (type === "newPassword") {
+      newState.newPassword = !isOpen.newPassword;
+      setIsOpen(newState);
+    }
+    if (type === "confirmPassword") {
+      newState.confirmPassword = !isOpen.confirmPassword;
+      setIsOpen(newState);
+    }
+  };
+
+  return (
+    <Box width="100%" height="100%" color="white">
+      <Center width="100%">
+        <Box w="100%" py={4} px={8}>
+          <Flex direction="column" gap={10} pt={4}>
+            <Box>
+              <Stack display="flex" direction="column" gap={5}>
+                <FormControl>
+                  <Stack
+                    display="flex"
+                    alignItems={{ base: "start", md: "center" }}
+                    justifyContent="space-between"
+                    direction={{ base: "column", md: "row" }}
+                  >
+                    <FormLabel
+                      display="block"
+                      textAlign="start"
+                      marginInlineEnd={3}
+                      transitionProperty="all"
+                      transitionDuration="150ms"
+                      minWidth="3xs"
+                      htmlFor="name"
+                    >
+                      <Box
+                        color="redanews"
+                        display="flex"
+                        alignItems="center"
+                        gap="2"
+                      >
+                        <Icon as={FiLock} strokeWidth="3px" w={5} h={5} />
+                        <Text fontWeight="bold" fontSize="lg">
+                          Original Password
+                        </Text>
+                      </Box>
+                    </FormLabel>
+                    <InputGroup>
+                      <Input
+                        type={isOpen.orgPassword ? "text" : "password"}
+                        id="org-password"
+                        label="org-password"
+                        placeholder="Original Password"
+                        value={orgPassword}
+                        onChange={(e) => setOrgPassword(e.target.value)}
+                        size="md"
+                        color="redanews"
+                        borderColor="black"
+                        focusBorderColor="primary.400"
+                        borderWidth="2px"
+                        borderRadius="5px"
+                        _hover={{ borderColor: "primary.400" }}
+                      />
+                      <InputRightElement>
+                        <IconButton
+                          onClick={() => onClickReveal("orgPassword")}
+                          icon={isOpen.orgPassword ? <HiEye /> : <HiEyeOff />}
+                          aria-label={
+                            isOpen ? "Mask password" : "Reveal password"
+                          }
+                          variant="link"
+                          border="none"
+                          _focus={{
+                            bg: "gray.100",
+                            border: "none",
+                            borderRadius: "full",
+                          }}
+                          marginRight="10px"
+                          paddingY="2px"
+                          fontSize="md"
+                          isRound
+                        />
+                      </InputRightElement>
+                    </InputGroup>
+                  </Stack>
+                </FormControl>
+                <FormControl>
+                  <Stack
+                    display="flex"
+                    alignItems={{ base: "start", md: "center" }}
+                    justifyContent="space-between"
+                    direction={{ base: "column", md: "row" }}
+                  >
+                    <FormLabel
+                      display="block"
+                      textAlign="start"
+                      marginInlineEnd={3}
+                      transitionProperty="all"
+                      transitionDuration="150ms"
+                      minWidth="3xs"
+                      htmlFor="name"
+                    >
+                      <Box
+                        color="redanews"
+                        display="flex"
+                        alignItems="center"
+                        gap="2"
+                      >
+                        <Icon as={FiLock} strokeWidth="3px" w={5} h={5} />
+                        <Text fontWeight="bold" fontSize="lg">
+                          New Password
+                        </Text>
+                      </Box>
+                    </FormLabel>
+                    <InputGroup>
+                      <Input
+                        type={isOpen.newPassword ? "text" : "password"}
+                        id="new-password"
+                        label="new-password"
+                        placeholder="New Password"
+                        value={newPassword}
+                        onChange={(e) => setNewPassword(e.target.value)}
+                        size="md"
+                        color="redanews"
+                        borderColor="black"
+                        focusBorderColor="primary.400"
+                        borderWidth="2px"
+                        borderRadius="5px"
+                        _hover={{ borderColor: "primary.400" }}
+                      />
+                      <InputRightElement>
+                        <IconButton
+                          onClick={() => onClickReveal("newPassword")}
+                          icon={isOpen.newPassword ? <HiEye /> : <HiEyeOff />}
+                          aria-label={
+                            isOpen ? "Mask password" : "Reveal password"
+                          }
+                          variant="link"
+                          border="none"
+                          _focus={{
+                            bg: "gray.100",
+                            border: "none",
+                            borderRadius: "full",
+                          }}
+                          marginRight="10px"
+                          paddingY="2px"
+                          fontSize="md"
+                          isRound
+                        />
+                      </InputRightElement>
+                    </InputGroup>
+                  </Stack>
+                </FormControl>
+                <FormControl>
+                  <Stack
+                    display="flex"
+                    alignItems={{ base: "start", md: "center" }}
+                    justifyContent="space-between"
+                    direction={{ base: "column", md: "row" }}
+                  >
+                    <FormLabel
+                      display="block"
+                      textAlign="start"
+                      marginInlineEnd={3}
+                      transitionProperty="all"
+                      transitionDuration="150ms"
+                      minWidth="3xs"
+                      htmlFor="name"
+                    >
+                      <Box
+                        color="redanews"
+                        display="flex"
+                        alignItems="center"
+                        gap="2"
+                      >
+                        <Icon as={FiCheck} strokeWidth="3px" w={5} h={5} />
+                        <Text fontWeight="bold" fontSize="lg">
+                          Confirm Password
+                        </Text>
+                      </Box>
+                    </FormLabel>
+                    <InputGroup>
+                      <Input
+                        type={isOpen.confirmPassword ? "text" : "password"}
+                        id="confirm-password"
+                        label="confirm-password"
+                        placeholder="Confirm New Password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        size="md"
+                        color="redanews"
+                        borderColor="black"
+                        focusBorderColor="primary.400"
+                        borderWidth="2px"
+                        borderRadius="5px"
+                        _hover={{ borderColor: "primary.400" }}
+                      />
+                      <InputRightElement>
+                        <IconButton
+                          onClick={() => onClickReveal("confirmPassword")}
+                          icon={
+                            isOpen.confirmPassword ? <HiEye /> : <HiEyeOff />
+                          }
+                          aria-label={
+                            isOpen ? "Mask password" : "Reveal password"
+                          }
+                          variant="link"
+                          border="none"
+                          _focus={{
+                            bg: "gray.100",
+                            border: "none",
+                            borderRadius: "full",
+                          }}
+                          marginRight="10px"
+                          paddingY="2px"
+                          fontSize="md"
+                          isRound
+                        />
+                      </InputRightElement>
+                    </InputGroup>
+                  </Stack>
+                </FormControl>
+              </Stack>
+            </Box>
+            <Flex direction="row-reverse" gap={4}>
+              <Button
+                size="lg"
+                bgColor="primary.600"
+                _hover={{
+                  backgroundColor: "redanews-teal",
+                  border: "0",
+                  color: "white",
+                }}
+                onClick={onSubmit}
+              >
+                Save
+              </Button>
+              <Button
+                size="lg"
+                bgColor="primary.300"
+                _hover={{
+                  backgroundColor: "primary.200",
+                  border: "0",
+                  color: "white",
+                }}
+                onClick={onCancel}
+              >
+                Cancel
+              </Button>
+            </Flex>
+          </Flex>
+        </Box>
+      </Center>
+    </Box>
+  );
+};
+
+export default PasswordSetting;
