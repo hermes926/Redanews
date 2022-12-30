470ca9dae2d1d80e9f35d3734024f77b263dfc47
ChienYu-1215 <b08705034@ntu.edu.tw>
2022-12-30 20:22:32 +0800

    [BE] Add patch user info

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -19,4 +19,24 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.patch("/:id", async (req, res) => {
+  if (mongoose.isValidObjectId(req.params.id)) {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      res.status(403).send({ message: "Unauthorized" });
+    } else {
+      if (req.body.username !== "" && req.body.username != undefined) {
+        user.username = req.body.username;
+      }
+      if (req.body.email !== "" && req.body.email != undefined) {
+        user.email = req.body.email;
+      }
+      await user.save();
+      res.status(200).send({ message: "OK" });
+    }
+  } else {
+    res.status(403).send({ message: "Unauthorized" });
+  }
+});
+
 export default router;
