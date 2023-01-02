ad2b3a3fe36fdbc06a7be7bd2b96ee309ab0d316
ChienYu-1215 <b08705034@ntu.edu.tw>
2023-01-02 15:08:04 +0800

    [FE] Refine history game views

--- frontend/src/Components/ui/Record.js
+++ frontend/src/Components/ui/Record.js
@@ -12,82 +12,16 @@ import {
   Link,
   Tag,
   HStack,
+  Stack,
 } from "@chakra-ui/react";
 
-import { useState, useEffect } from "react";
-import { commonWords, marks } from "../../Containers/utils/variables";
-import axios from "../../api";
-
 const Record = ({ history, recordOpen, recordOpenClick }) => {
-  //const content = "'happy birthday' to you happy birthday, to you happy birthday to you!"
-  //const guesses = ["happy", "birthday"]
-  const [content, setContent] = useState("");
-  const [title, setTitle] = useState("");
-  const [article, setArticle] = useState("");
-
-  const makeRecord = (contentt) => {
-    //copy from redact.js
-    const guesses = history.vocabs;
-
-    let redacted = []; //this is new redact algorithm
-    const words = contentt.split(
-      /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~\n\s’‘–]+/
-    );
-    let words_index = words[0] === "" ? 1 : 0;
-    for (let i = 0; i < contentt.length; i++) {
-      if (/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~\n\s’‘–]/.test(contentt[i])) {
-        redacted.push(contentt[i]);
-      } else {
-        if (
-          guesses.find(
-            (guess) => guess.toLowerCase() === words[words_index].toLowerCase()
-          )
-        ) {
-          redacted.push(
-            <span key={i * 2} style={{ fontWeight: "bolder" }}>
-              {words[words_index]}
-            </span>
-          ); //for guessed words, text are thicker.
-        } else if (
-          commonWords.find(
-            (commonWord) => commonWord === words[words_index].toLowerCase()
-          )
-        ) {
-          redacted.push(<span key={i * 2}>{words[words_index]}</span>); //for given words, text are normal.
-        } else {
-          redacted.push(
-            <span key={i * 2} style={{ color: "#FFC9C9" }}>
-              {words[words_index]}
-            </span>
-          ); //for words not being guessed, text are red.
-        }
-        i += words[words_index].length - 1;
-        words_index += 1;
-      }
-    }
-    return redacted;
-  };
-
-  useEffect(() => {
-    if (content != "") {
-      setArticle(makeRecord(content));
-      setTitle(makeRecord(history.newsTitle));
-    }
-  }, [content]);
-
-  useEffect(() => {
-    if (recordOpen) {
-      getArtitle();
-    }
-  }, [recordOpen]);
-
   return (
     <Modal
       isOpen={recordOpen}
       onClose={() => {
         recordOpenClick();
       }}
-      size="4xl"
     >
       <ModalOverlay />
       <ModalContent>
@@ -96,26 +30,51 @@ const Record = ({ history, recordOpen, recordOpenClick }) => {
         </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <HStack justify="space-between">
-            <HStack spacing="2">
+          <Stack spacing="5">
+            <HStack>
+              <Text fontWeight="700">Status :</Text>
+              {history.win ? (
+                <Tag size="md" colorScheme="red">
+                  SUCCESS
+                </Tag>
+              ) : (
+                <Tag size="md" colorScheme="red">
+                  FAILED
+                </Tag>
+              )}
+            </HStack>
+            <Stack spacing="2">
               <Text fontWeight="700">News Title : </Text>
-              <Link as="button" color="teal" href={history.newsLink} isExternal>
+              <Link
+                color="teal"
+                _hover={{
+                  color: "redanews-blue",
+                  cursor: "pointer",
+                  textDecoration: "underline",
+                }}
+                href={history.newsLink}
+                align="left"
+                isExternal
+              >
                 {history.newsTitle}
               </Link>
+            </Stack>
+
+            <HStack spacing="2">
+              <Text fontWeight="700">Your Game : </Text>
+              <Link
+                color="teal"
+                _hover={{
+                  color: "redanews-blue",
+                  cursor: "pointer",
+                  textDecoration: "underline",
+                }}
+                href={"/archives/" + history.news_id + "/" + history.guess_id}
+              >
+                Click to view{" "}
+              </Link>
             </HStack>
-            {history.win ? (
-              <Tag size="md" colorScheme="red">
-                SUCCESS
-              </Tag>
-            ) : (
-              <Tag size="md" colorScheme="red">
-                FAILED
-              </Tag>
-            )}
-          </HStack>
-          <Text fontWeight="700">Your Guessing Progress : </Text>
-          <Heading>{title}</Heading>
-          {article}
+          </Stack>
         </ModalBody>
 
         <ModalFooter>
