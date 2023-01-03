23f002422c7d85fb684b9467c162635dbdf780d9
ChienYu-1215 <b08705034@ntu.edu.tw>
2023-01-03 09:42:10 +0800

    [FE] Deployment related update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `http://localhost:4000`,
+  baseURL: `https://redanews.df.r.appspot.com/`,
 });
 
 export default instance;
